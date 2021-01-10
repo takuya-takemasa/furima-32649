44b2612a80c9bc355a4a61dcd1cfd737d9b424c0
takuya-takemasa <takemasa11821182@gmail.com>
2021-01-10 18:11:03 +0900

    商品出品機能　提出２回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :authenticate_user!, except: [:new, :create, :show]
   
   def index 
     @items = Item.all.order("created_at DESC")
