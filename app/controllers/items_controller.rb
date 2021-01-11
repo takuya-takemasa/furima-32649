abe29c22252a000cd8ffddde0599097b0fd056d1
takuya-takemasa <takemasa11821182@gmail.com>
2021-01-11 17:30:20 +0900

    商品情報編集　提出３回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,17 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
   
   
   
   def update
-    @item = Item.find(params[:id])
     if @item.update(items_params)
       redirect_to root_path
     else
